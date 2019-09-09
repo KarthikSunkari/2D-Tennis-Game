067e1cb7849393425c334bda9c0362c023501d1c
KarthikSunkari <sk65@iitbbs.ac.in>
2019-09-09 17:55:39 +0530

    Better Computer Movement

--- script.js
+++ script.js
@@ -1,93 +1,153 @@
-	var ballx = 350;var ballSpeedx = 10;
-		var bally = 250;var ballSpeedy = 5;
+const initialspeedX = 7.5;		//Initial Speeds
+const initialspeedY = 5;
 
-		var fps = 30;		//Frames per sec
+var ballx = 350;
+var ballSpeedx = initialspeedX;
 
-		var canvas = document.getElementById("CanvasGame");
-		var	canvasContext = canvas.getContext('2d');
+var bally = 250;
+var ballSpeedy = initialspeedY;
 
-		var pad1x = 0;			//Player
-		var pad1y = 220;
+var fps = 60;		//Frames per sec
 
-		var pad2x = 690;		//Computer
-		var pad2y = 220;
+var canvas = document.getElementById("CanvasGame");
+var	canvasContext = canvas.getContext('2d');
+var scoreUpdate = document.getElementById("ScoreCard");
+var	scoreContext = scoreUpdate.getContext('2d');
 
-		var ballradius = 5;
-		var paddleheight = 60;
-		var paddlewidth = 10
-		var e = 0.35;
+var pad1x = 0;			//Player
+var pad1y = 250;
 
-		function calculateMousePos(evt){
-			var rect = canvas.getBoundingClientRect();
-			var root = document.documentElement;
-			var mouseX = evt.clientX - rect.left - root.scrollLeft;
-			var mouseY = evt.clientY - rect.top - root.scrollTop;
-			return{
-				x : mouseX,
-				y : mouseY
-			};
+var pad2x = 690;		//Computer
+var pad2y = 250;
+
+var ballradius = 5;		//Ball Dimensions
+
+const paddleheight = 80;	//Paddle Dimensions
+const paddlewidth = 10;
+
+var e = 0.35*6;
+var speedMultiplier=1.009;		//Speed Enhancers
+
+var xScore=0;
+var yScore=0;
+
+function calculateMousePos(evt){
+	var rect = canvas.getBoundingClientRect();
+	var root = document.documentElement;
+	var mouseX = evt.clientX - rect.left - root.scrollLeft;
+	var mouseY = evt.clientY - rect.top - root.scrollTop;
+	return{
+		x : mouseX,
+		y : mouseY
+		};
+}
+
+window.onload = function () {
+	console.log("Hello");
+	setInterval(myfunc,1000/fps);
+
+	canvas.addEventListener('mousemove',
+		function(evt){
+			//pad1x=calculateMousePos(evt).x;
+			pad1y=calculateMousePos(evt).y;
 		}
+		);
+}
 
-		window.onload = function () {
-			console.log("Hello");
-			setInterval(myfunc,1000/fps);
+function ballReset(){
+	ballx = canvas.width/2;
+	bally = canvas.height/2;
+	if(ballSpeedx<0)
+	ballSpeedx = -initialspeedX;
+	else
+	ballSpeedx = initialspeedY;
+	ballSpeedy = 0;
+}
 
-			canvas.addEventListener('mousemove',
-				function(evt){
-					//pad1x=calculateMousePos(evt).x;
-					pad1y=calculateMousePos(evt).y;
-				}
-				);
+function scoreReset(){
+
+}
+
+function ComputerMov(){
+	if(ballSpeedx>0){
+		if(Math.abs(ballSpeedy)>5){
+			if(pad2y<=bally)
+				pad2y+=6;
+			else if(pad2y>bally)
+				pad2y-=6;
 		}
+		else if(Math.abs(ballSpeedy)!=0){
+			if(pad2y<=bally)
+				pad2y+=1.5;
+			else if(pad2y>bally)
+				pad2y-=1.5;
+		}
+		else{
+			pad2y=bally;
+		}
+		console.log(pad2y + "-" + bally);
+	}
 
-		function ballReset(){
-			ballx = canvas.width/2;
-			bally = canvas.height/2;
+}
+
+function myfunc(){
+
+	ComputerMov();
+
+	if (ballx+paddlewidth>=canvas.width-1.2*ballradius){
+		if(bally+ballradius>=pad2y-paddleheight/2 && bally-ballradius<=pad2y+paddleheight/2){
+			ballSpeedx = -speedMultiplier*ballSpeedx;
+			ballSpeedy = speedMultiplier*Math.cbrt(bally-pad1y)*e;
+		}
+		else{
+			ballReset();
+			xScore++;
+		}
+	}
+
+	if(ballx<=1.2*ballradius+paddlewidth) {
+		if(bally+2*ballradius>=pad1y-paddleheight/2 && bally-2*ballradius<=pad1y+paddleheight/2){
 			ballSpeedx = -ballSpeedx;
-			ballSpeedy = 5;
+			ballSpeedy = Math.cbrt(bally-pad1y)*e;
 		}
-
-		function myfunc(){
-				if (ballx+paddlewidth>=canvas.width-ballradius){
-					if(bally+ballradius>=pad2y-paddleheight/2 && bally-ballradius<=pad2y+paddleheight/2){
-						ballSpeedx = -ballSpeedx;
-						ballSpeedy = (bally -pad2y)*e;
-					}
-				else
-					ballReset();
-				}
-
-				if(ballx<=ballradius+paddlewidth) {
-					if(bally+ballradius>=pad1y-paddleheight/2 && bally-ballradius<=pad1y+paddleheight/2){
-						ballSpeedx = -ballSpeedx;
-						ballSpeedy = (bally -pad1y)*e;
-					}
-				else
-					ballReset();
-				}
+		else{
+			ballReset();
+			yScore++;
+		}
+	}
 			
-			if (bally>canvas.height-ballradius || bally<ballradius) {
-				ballSpeedy = -ballSpeedy;
-			}
+	if (bally>canvas.height-ballradius || bally<ballradius) {
+		ballSpeedy = -ballSpeedy;
+		console.log(bally);
+	}
 
-			//console.log(ballx + "*" + bally);
-			//console.log(ballSpeedy);
-			console.log(bally+"-"+pad2y);
-			canvasContext.fillStyle="black";
-			canvasContext.fillRect(0,0,canvas.width,canvas.height);
-			//Player
-			canvasContext.fillStyle = 'white';
-			canvasContext.fillRect(pad1x,pad1y-paddleheight/2,paddlewidth,paddleheight);
-			//Computer
-			pad2y = bally;
-			canvasContext.fillStyle = 'white';
-			canvasContext.fillRect(pad2x,pad2y-paddleheight/2,paddlewidth,paddleheight);
-			//ball
-			canvasContext.fillStyle='red';
-			canvasContext.beginPath();
-			canvasContext.arc(ballx,bally,ballradius,0,Math.PI*2,true);
-			canvasContext.fill();
+	//background
+	canvasContext.fillStyle="black";
+	canvasContext.fillRect(0,0,canvas.width,canvas.height);
 
-			ballx+=ballSpeedx;
-			bally+=ballSpeedy;
-		}
+	//Score
+	scoreContext.font = "20px Arial";
+	scoreContext.fillStyle = "red";
+	scoreContext.strokeText("xScore",10,40);
+	//scoreContext.strokeText(yScore,300,330);
+
+	//Player
+	canvasContext.fillStyle = 'white';
+	canvasContext.fillRect(pad1x,pad1y-paddleheight/2,paddlewidth,paddleheight);
+
+	//Computer
+	canvasContext.fillStyle = 'white';
+	canvasContext.fillRect(pad2x,pad2y-paddleheight/2,paddlewidth,paddleheight);
+
+	//ball
+	canvasContext.fillStyle='red';
+	canvasContext.beginPath();
+	canvasContext.arc(ballx,bally,ballradius,0,Math.PI*2,true);
+	canvasContext.fill();
+
+	//Position Update 
+	ballx+=ballSpeedx;
+	bally+=ballSpeedy;
+
+
+}
